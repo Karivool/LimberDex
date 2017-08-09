226b96468c845b2bba02251b20c8074a5bd98599
Karivool <themataaa@gmail.com>
2017-08-08 22:14:16 -0400

    Add more methods

--- public/main.rb
+++ public/main.rb
@@ -4,5 +4,21 @@ require 'open-uri'
 
 def ParseURL(url)
   page = Nokogiri::HTML(open(url.to_s))
-  page.class   # => Nokogiri::HTML::Document
+  # page.class   # => Nokogiri::HTML::Document
+  GetElements(page)
+end
+
+def ParsePage(element)
+  container = []
+  element.each { |ele| container.push(ele) }
+  container
+end
+
+def GetElements(page)
+  elements = []
+  elements.push(page.css("h1"))
+  elements.push(page.css("h2"))
+  elements.push(page.css("h3"))
+  elements.push(page.css("a"))
+  elements
 end
